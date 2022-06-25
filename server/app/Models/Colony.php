32d85f75904d0a148b69b699137d621bbfac9140
Лебедев Александр <god@lifegame.su>
2022-06-25 08:26:29 +0300

    added civilization to colony, colony getColumns

--- server/app/Models/Colony.php
+++ server/app/Models/Colony.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Colony extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'colony_type_id',
+        'space_object_id',
+        'civilization_id'
+    ];
+
+    function getColumns()
+    {
+
+        $colonyTypes = [];
+        foreach (ColonyType::get() as $colonyType) {
+            $colonyTypes[] = [
+                'value' => $colonyType->id,
+                'label' => $colonyType->runame
+            ];
+        }
+
+        $spaceObjects = [];
+        foreach (SpaceObject::get() as $spaceObject) {
+            $spaceObjects[] = [
+                'value' => $spaceObject->id,
+                'label' => $spaceObject->name
+            ];
+        }
+
+        return [
+            'name' => ['Название колонии', 'text'],
+            'colony_type_id' => ['Тип колонии', 'select', $colonyTypes],
+            'name' => ['Название колонии', 'select', $spaceObjects],
+        ];
+    }
 }
